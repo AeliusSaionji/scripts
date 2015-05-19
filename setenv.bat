0172a33c2144da3a09d6c3f6ec24c14cf819886b
Link <link-satonaka@outlook.com>
2015-05-18 23:01:58 -0400

    fixes

--- setenv.bat
+++ setenv.bat
@@ -3,6 +3,9 @@ MKDIR %HOMEDRIVE%\cygwin64\home\%USERNAME%
 IF %COMPUTERNAME% EQU NERV (
 	set My_Path=D:
 	MKLINK /J "%HOMEDRIVE%\cygwin64\home\%USERNAME%\dcc" "D:\Downloads\dcc"
+	set GitHub=%My_Path%\GitHub
+	COPY %GitHub%\scripts\General.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\startup
+	COPY %GitHub%\scripts\rsyncd.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\startup
 ) else (
 	set My_Path=%HOMEDRIVE%%HOMEPATH%
 	MKDIR %HOMEDRIVE%%HOMEPATH%\Desktop\dcc
@@ -21,10 +24,7 @@ setx Music %My_Path%\Music
 setx OneDrive %My_Path%\OneDrive
 setx Pictures %My_Path%\Pictures
 setx Videos %My_Path%\Videos
-set GitHub=%My_Path%\GitHub
 set OneDrive=%My_Path%\OneDrive
-COPY %GitHub%\scripts\General.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\startup
-COPY %GitHub%\scripts\rsyncd.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\startup
 MKLINK /J "%HOMEDRIVE%\cygwin64\home\%USERNAME%\.irssi" "%ONEDRIVE%\Misc Utils\irssi\.irssi"
 MKLINK /J "%HOMEDRIVE%%HOMEPATH%\pentadactyl" "%ONEDRIVE%\Misc Utils\Firefox backup\pentadactyl"
 PowerShell -Command Start-Process "\"PowerShell\" \"Copy-Item $Env:OneDrive\MISCUT~1\ELEVAT~1.0-R\BIN~2.X86\elevate.exe -Destination C:\\Windows\"" -Verb runas
