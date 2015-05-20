800b6e712f82a480d3836c21adaeefaf016a73a5
Link-Satonaka <Link-Satonaka@outlook.com>
2015-05-20 04:15:20 -0400

    geforce experience

--- silentinstall.ps1
+++ silentinstall.ps1
@@ -59,16 +59,18 @@ Write-Host "Hamachi's fucking MSI is fucking retarded so I will not be using the
 
 Write-Host "Downloading and installing GitHub, Honeyview, Vistaswitcher, and Vtuploader..."
 Write-Warning "GitHub, Honeyview, Vistaswitcher, and Vtuploader don't have silent installs"
+	$webclient.DownloadFile( "http://us.download.nvidia.com/GFE/GFEClient/2.4.3.31/GeForce_Experience_v2.4.3.31.exe", "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe" )
 	$webclient.DownloadFile( "https://github-windows.s3.amazonaws.com/GitHubSetup.exe", "$ENV:TEMP\GitHubSetup.exe" )
 	$webclient.DownloadFile( "http://www.bandisoft.com/honeyview/dl.php?web", "$Env:TEMP\honeyview.exe" )
-    $webclient.DownloadFile( "http://download01.logitech.com/web/ftp/pub/techsupport/gaming/LGS_8.58.183_x64_Logitech.exe", "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe" )
+	$webclient.DownloadFile( "http://download01.logitech.com/web/ftp/pub/techsupport/gaming/LGS_8.58.183_x64_Logitech.exe", "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe" )
 	$webclient.DownloadFile( "http://www.ntwind.com/download/VistaSwitcher_1.1.5-setup.exe", "$Env:TEMP\VistaSwitcher_1.1.5-setup.exe" )
 	$webclient.DownloadFile( "https://www.virustotal.com/static/bin/vtuploader2.2.exe", "$Env:TEMP\vtuploader2.2.exe" )
 
 # No silent yet
+	Start-Process "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe"
 	Start-Process "$ENV:TEMP\GitHubSetup.exe"
 	Start-Process "$ENV:TEMP\honeyview.exe"
-    Start-Process "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe"
+	Start-Process "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe"
 	Start-Process "$Env:TEMP\VistaSwitcher_1.1.5-setup.exe"
 	Start-Process "$Env:TEMP\vtuploader2.2.exe"
 }
