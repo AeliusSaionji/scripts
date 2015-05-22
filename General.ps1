60cd83a8be4c345493c33ff3e248003077456d71
Link-Satonaka <Link-Satonaka@outlook.com>
2015-05-22 18:47:43 -0400

    finished rewriting in UIAutomation

--- General.ps1
+++ General.ps1
@@ -1,13 +1,19 @@
 # lastpass fingerprint scanner fix
 Import-Module UIAutomation
 [UIAutomation.Mode]::Profile = [UIAutomation.Modes]::Normal;
+
+Function detect-lp {
+	Wait-UiaWindow -Name "Swipe finger" -Class "#32770" -Timeout 99999 -OutVariable lpfinger
+	if ($lpfinger -eq "True") {
+		Invoke-UiaHotkey -Key t -Win
+		Invoke-UiaHotkey -Key Esc
+		Get-UiaWindow -Name "Swipe finger" -Class "#32770" -OutVariable lpfinger | Set-UiaFocus
+		do {
+			Start-Sleep 5
+		} until ($lpfinger.WindowInteractionState -match "Closing")
+	}
+}
+
 do {
-	Wait-UiaWindow -Name "Swipe finger" -Class '#32770' -Timeout 99999
-	Invoke-UiaHotkey -Key t -Win
-	Invoke-UiaHotkey -Key Esc
-	Get-UiaWindow -ProcessName firefox -OutVariable firefox
-	Get-UiaWindow -Name "Swipe finger" -Class "#32770" | Set-UiaFocus
-	do {
-		Start-Sleep 5
-	} until ($firefox.WindowInteractionState -match "ReadyForUserInteraction")
-} while ($true)
+	detect-lp
+} while($true)
