37409241ae5abbba88ef002cccef672d93571050
Link-Satonaka <Link-Satonaka@outlook.com>
2015-05-22 05:15:17 -0400

    why did this take so long

--- General.ps1
+++ General.ps1
@@ -1,11 +1,13 @@
 # lastpass fingerprint scanner fix
+Import-Module UIAutomation
+[UIAutomation.Mode]::Profile = [UIAutomation.Modes]::Normal;
 do {
+	Wait-UiaWindow -Name "Swipe finger" -Class '#32770' -Timeout 99999
+	Invoke-UiaHotkey -Key t -Win
+	Invoke-UiaHotkey -Key Esc
+	Get-UiaWindow -ProcessName firefox -OutVariable firefox
+	Get-UiaWindow -Name "Swipe finger" -Class "#32770" | Set-UiaFocus
 	do {
-		$fpwindow = Select-Window firefox | Select-ChildWindow;Start-Sleep 2
-	} until ($fpwindow.Title -match "Swipe finger")
-	Select-Window firefox | Set-WindowActive
-	Select-Window firefox | Select-ChildWindow | Set-WindowActive
-	do {
-		$fpwindow = Select-Window firefox | Select-ChildWindow;Start-Sleep 2
-	} while ($fpwindow.Title -match "Swipe finger")
+		Start-Sleep 5
+	} until ($firefox.WindowInteractionState -match "ReadyForUserInteraction")
 } while ($true)
