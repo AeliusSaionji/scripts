46363c7fecdf60ecad52cae133c18a1504afdc9c
Link-Satonaka <Link-Satonaka@outlook.com>
2015-05-21 21:22:49 -0400

    many syntax and command fixes

--- silentinstall.ps1
+++ silentinstall.ps1
@@ -6,10 +6,8 @@
 # 	Shortcut transfer from root to global
 #   AHK's thingy for admin stuff
 #   WinX
-# mpc hc, irfanview borked
 # revo uninstaller
 # gvimx64, freecommander not covered here
-# 	git installs vim: AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\share\vim\vim74
 # set wallpaper
 # set taskbar settings
 # hide desktop icons
@@ -19,6 +17,7 @@
 if ( -NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") ) {
 	Write-Host "Updating Shell Paths"
 	Write-Warning "Downloads, Saved Games, and Searches must be set manually, probably."
+	# Replace with WASP?
 		$key1 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
 		$key2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
 		set-ItemProperty -path $key1 -name AppData "H:\Roaming"
@@ -33,77 +32,71 @@ if ( -NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIden
 		set-ItemProperty -path $key2 -name "My Video" "H:\Videos"
 		set-ItemProperty -path $key1 -name "Personal" "H:\OneDrive\Documents"
 		set-ItemProperty -path $key2 -name "Personal" "H:\OneDrive\Documents"
-	COPY .\silentinstall.ps1 $ENV:PUBLIC
-	Start-Process "powershell" "$ENV:PUBLIC\silentinstall.ps1" -Verb Runas
+	$getdir = Get-Location
+	Start-Process powershell.exe -ArgumentList "$getdir\silentinstall.ps1" -Verb Runas
 }
 else {
-$webclient = New-Object System.Net.WebClient
-Write-Host "Downloading and installing hashcheck..."
-	$webclient.DownloadFile( "http://code.kliu.org/hashcheck/downloads/HashCheckInstall-latest.exe", "$ENV:TEMP\HashCheckInstall-latest.exe" )
-	Start-Process "$ENV:TEMP\HashCheckInstall-latest.exe" "/quiet"
-Write-Host "Downloading and installing vcredist..."
-	$webclient.DownloadFile( "http://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE", "$ENV:TEMP\vcredist_x64.exe" )
-	$webclient.DownloadFile( "http://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE", "$ENV:TEMP\vcredist_x86.exe" )
-	Start-Process "$ENV:TEMP\vcredist_x64.EXE" "/Q" -wait
-	Start-Process "$ENV:TEMP\vcredist_x86.EXE" "/Q" -wait
-Write-Host "Downloading and installing hardlink shell extension (explorer will restart)..."
-	$webclient.DownloadFile( "http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_X64.exe", "$ENV:TEMP\HardLinkShellExt_X64.exe" )
-	Start-Process "$ENV:TEMP\HardLinkShellExt_X64.exe" "/S /Language=English"
-Write-Host "Downloading and installing sumatrapdf..."
-	$webclient.DownloadFile( "https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-3.0-install.exe", "$ENV:TEMP\SumatraPDF-3.0-install.exe" )
-	Start-Process "$ENV:TEMP\SumatraPDF-3.0-install.exe" "/s /register"
-Write-Host "Downloading and installing AHK..."
-	$webclient.DownloadFile( "http://ahkscript.org/download/ahk-install.exe", "$ENV:TEMP\ahk-install.exe" )
-	Start-Process "$ENV:TEMP\ahk-install.exe" "/S /U64"
-Write-Host "Downloading and installing AltDrag..."
-	$webclient.DownloadFile( "https://github.com/stefansundin/altdrag/releases/download/v1.0/AltDrag-1.0.exe", "$ENV:TEMP\AltDrag-1.0.exe" )
-	Start-Process "$ENV:TEMP\AltDrag-1.0.exe" "/S"
-Write-Host "Downloading and installing ProcessHacker..."
-	$webclient.DownloadFile( "http://processhacker.googlecode.com/files/processhacker-2.33-setup.exe", "$ENV:TEMP\processhacker-2.33-setup.exe" )
-	Start-Process "$ENV:TEMP\processhacker-2.33-setup.exe" "/SILENT"
-Write-Host "Downloading and installing Steam..."
-	$webclient.DownloadFile( "http://media.steampowered.com/client/installer/SteamSetup.exe", "$ENV:TEMP\SteamSetup.exe" )
-	Start-Process "$ENV:TEMP\SteamSetup.exe" "/S"
-Write-Host "Downloading and installing directx..."
-	$webclient.DownloadFile( "http://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe", "$ENV:TEMP\dxwebsetup.exe" )
-	Start-Process "$ENV:TEMP\dxwebsetup.exe" "/Q"
-Write-Host "Downloading and installing MPC-HC x86..." # x86 for SVP
-	$webclient.DownloadFile( "http://superb-dca3.dl.sourceforge.net/project/mpc-hc/MPC%20HomeCinema%20-%20Win32/MPC-HC_v1.7.7_x86/MPC-HC.1.7.7.x86.exe", "$ENV:TEMP\MPC-HC.1.7.7.x86.exe" )
-	Start-Process "$ENV:TEMP\MPC-HC.1.7.7.x86.exe" "/SILENT"
-Write-Host "Downloading and installing Ditto..."
-	$webclient.DownloadFile( "http://downloads.sourceforge.net/project/ditto-cp/Ditto/3.20.54.0/DittoSetup_64bit_3_20_54_0.exe", "$ENV:TEMP\DittoSetup_64bit_3_20_54_0.exe" )
-	Start-Process "$ENV:TEMP\DittoSetup_64bit_3_20_54_0.exe" "/SILENT"
-Write-Host "Downloading and installing irfanview 4.38..."
-	$webclient.DownloadFile( "http://filehippo.com/download/file/c1fd292cac18c899909fd45098bfe9e636271c70111cdf484635b78e267521e0/", "$ENV:TEMP\irfanview438.exe" )
-	Start-Process "$ENV:TEMP\irfanview438.exe" "/silent /group=1 /allusers=1 /assocallusers=1"
-Write-Host "Downloading and installing paint.net..."
-	$webclient.DownloadFile( "http://www.dotpdn.com/files/paint.net.4.0.5.install.zip", "$ENV:TEMP\paint.net.4.0.5.install.zip" )
-	Add-Type -assembly "system.io.compression.filesystem"
-	[io.compression.zipfile]::ExtractToDirectory("$ENV:TEMP\paint.net.4.0.5.install.zip", "$ENV:TEMP")
-	Start-Process "$ENV:TEMP\paint.net.4.0.5.install.exe" "/auto"
-
-Write-Host "Hamachi's fucking MSI is fucking retarded so I will not be using the fucking silent option and apparently it fucking pings google every time you install it"
-
-Write-Host "Downloading and installing GitHub, Honeyview, Vistaswitcher, and Vtuploader..."
-Write-Warning "Firefox, foobar2000, Geforce Experience, GitHub, Honeyview, Vistaswitcher, and Vtuploader don't have silent installs"
-	$webclient.DownloadFile( "https://download-installer.cdn.mozilla.net/pub/firefox/releases/38.0.1/win32/en-US/Firefox%20Setup%20Stub%2038.0.1.exe", "$ENV:TEMP\Firefox Setup Stub 38.0.1.exe" )
-	$webclient.DownloadFile( "http://www.foobar2000.org/getfile/5ade2dfcc3b9483f33cfff60cf8189e7/foobar2000_v1.3.8.exe", "$ENV:TEMP\foobar2000_v1.3.8.exe" )
-	$webclient.DownloadFile( "http://us.download.nvidia.com/GFE/GFEClient/2.4.3.31/GeForce_Experience_v2.4.3.31.exe", "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe" )
-	$webclient.DownloadFile( "https://github-windows.s3.amazonaws.com/GitHubSetup.exe", "$ENV:TEMP\GitHubSetup.exe" )
-	$webclient.DownloadFile( "http://www.bandisoft.com/honeyview/dl.php?web", "$ENV:TEMP\honeyview.exe" )
-	$webclient.DownloadFile( "http://download01.logitech.com/web/ftp/pub/techsupport/gaming/LGS_8.58.183_x64_Logitech.exe", "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe" )
-	$webclient.DownloadFile( "http://www.ntwind.com/download/VistaSwitcher_1.1.5-setup.exe", "$ENV:TEMP\VistaSwitcher_1.1.5-setup.exe" )
-	$webclient.DownloadFile( "https://www.virustotal.com/static/bin/vtuploader2.2.exe", "$ENV:TEMP\vtuploader2.2.exe" )
-
-# No silent yet
-	Start-Process "$ENV:TEMP\Firefox Setup Stub 38.0.1.exe"
-	Start-Process "$ENV:TEMP\foobar2000_v1.3.8.exe"
-	Start-Process "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe"
-	Start-Process "$ENV:TEMP\GitHubSetup.exe"
-	Start-Process "$ENV:TEMP\honeyview.exe"
-	Start-Process "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe"
-	Start-Process "$ENV:TEMP\VistaSwitcher_1.1.5-setup.exe"
-	Start-Process "$ENV:TEMP\vtuploader2.2.exe"
+	Write-Host "Downloading and installing hashcheck..."
+		Invoke-WebRequest -UseBasicParsing "http://code.kliu.org/hashcheck/downloads/HashCheckInstall-latest.exe" -OutFile "$ENV:TEMP\HashCheckInstall-latest.exe"
+		Start-Process "$ENV:TEMP\HashCheckInstall-latest.exe" "/quiet"
+	Write-Host "Downloading and installing vcredist..."
+		Invoke-WebRequest -UseBasicParsing "http://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE" -OutFile "$ENV:TEMP\vcredist_x64.exe"
+		Invoke-WebRequest -UseBasicParsing "http://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE" -OutFile "$ENV:TEMP\vcredist_x86.exe"
+		Start-Process "$ENV:TEMP\vcredist_x64.EXE" "/Q" -wait
+		Start-Process "$ENV:TEMP\vcredist_x86.EXE" "/Q" -wait
+	Write-Host "Downloading and installing hardlink shell extension (explorer will restart)..."
+		Invoke-WebRequest -UseBasicParsing "http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_X64.exe" -OutFile "$ENV:TEMP\HardLinkShellExt_X64.exe"
+		Start-Process "$ENV:TEMP\HardLinkShellExt_X64.exe" "/S /Language=English"
+	Write-Host "Downloading and installing sumatrapdf..."
+		Invoke-WebRequest -UseBasicParsing "https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-3.0-install.exe" -OutFile "$ENV:TEMP\SumatraPDF-3.0-install.exe"
+		Start-Process "$ENV:TEMP\SumatraPDF-3.0-install.exe" "/s /register"
+	Write-Host "Downloading and installing AHK..."
+		Invoke-WebRequest -UseBasicParsing "http://ahkscript.org/download/ahk-install.exe" -OutFile "$ENV:TEMP\ahk-install.exe"
+		Start-Process "$ENV:TEMP\ahk-install.exe" "/S /U64"
+	Write-Host "Downloading and installing AltDrag..."
+		Invoke-WebRequest -UseBasicParsing "https://github.com/stefansundin/altdrag/releases/download/v1.0/AltDrag-1.0.exe" -OutFile "$ENV:TEMP\AltDrag-1.0.exe"
+		Start-Process "$ENV:TEMP\AltDrag-1.0.exe" "/S"
+	Write-Host "Downloading and installing ProcessHacker..."
+		Invoke-WebRequest -UseBasicParsing "http://processhacker.googlecode.com/files/processhacker-2.33-setup.exe" -OutFile "$ENV:TEMP\processhacker-2.33-setup.exe"
+		Start-Process "$ENV:TEMP\processhacker-2.33-setup.exe" "/SILENT"
+	Write-Host "Downloading and installing Steam..."
+		Invoke-WebRequest -UseBasicParsing "http://media.steampowered.com/client/installer/SteamSetup.exe" -OutFile "$ENV:TEMP\SteamSetup.exe"
+		Start-Process "$ENV:TEMP\SteamSetup.exe" "/S"
+	Write-Host "Downloading and installing directx..."
+		Invoke-WebRequest -UseBasicParsing "http://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe" -OutFile "$ENV:TEMP\dxwebsetup.exe"
+		Start-Process "$ENV:TEMP\dxwebsetup.exe" "/Q"
+	#Write-Host "Downloading and installing MPC-HC..." # x86 for SVP
+	#	Start-Process "$ENV:TEMP\MPC-HC_v1.7.8_x64" "/SILENT"
+	#Write-Host "Downloading and installing Ditto..."
+	#	Start-Process "$ENV:TEMP\DittoSetup_64bit_3_20_54_0.exe" "/SILENT"
+	# Write-Host "Downloading and installing irfanview 4.38..."
+	#	Start-Process "$ENV:TEMP\irfanview438.exe" "/silent /group=1 /allusers=1 /assocallusers=1"
+	Write-Host "Downloading and installing paint.net..."
+		Invoke-WebRequest -UseBasicParsing "http://www.dotpdn.com/files/paint.net.4.0.5.install.zip" -OutFile "$ENV:TEMP\paint.net.4.0.5.install.zip"
+		Add-Type -assembly "system.io.compression.filesystem"
+		[io.compression.zipfile]::ExtractToDirectory("$ENV:TEMP\paint.net.4.0.5.install.zip", "$ENV:TEMP")
+		Start-Process "$ENV:TEMP\paint.net.4.0.5.install.exe" "/auto"
+	
+	Write-Host "Hamachi's fucking MSI is fucking retarded so I will not be using the fucking silent option and apparently it fucking pings google every time you install it"
+	
+	Write-Host "Downloading and installing GitHub, Honeyview, Vistaswitcher, and Vtuploader..."
+	Write-Warning "Firefox, foobar2000, Geforce Experience, GitHub, Honeyview, Vistaswitcher, and Vtuploader don't have silent installs"
+		Invoke-WebRequest -UseBasicParsing "https://download-installer.cdn.mozilla.net/pub/firefox/releases/38.0.1/win32/en-US/Firefox%20Setup%20Stub%2038.0.1.exe" -OutFile "$ENV:TEMP\Firefox Setup Stub 38.0.1.exe"
+	#	Invoke-WebRequest -UseBasicParsing "http://www.foobar2000.org/getfile/5ade2dfcc3b9483f33cfff60cf8189e7/foobar2000_v1.3.8.exe" -OutFile "$ENV:TEMP\foobar2000_v1.3.8.exe"
+		Invoke-WebRequest -UseBasicParsing "http://us.download.nvidia.com/GFE/GFEClient/2.4.3.31/GeForce_Experience_v2.4.3.31.exe" -OutFile "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe"
+		Invoke-WebRequest -UseBasicParsing "https://github-windows.s3.amazonaws.com/GitHubSetup.exe" -OutFile "$ENV:TEMP\GitHubSetup.exe"
+		Invoke-WebRequest -UseBasicParsing "http://www.bandisoft.com/honeyview/dl.php?web" -OutFile "$ENV:TEMP\honeyview.exe"
+		Invoke-WebRequest -UseBasicParsing "http://download01.logitech.com/web/ftp/pub/techsupport/gaming/LGS_8.58.183_x64_Logitech.exe" -OutFile "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe"
+		Invoke-WebRequest -UseBasicParsing "http://www.ntwind.com/download/VistaSwitcher_1.1.5-setup.exe" -OutFile "$ENV:TEMP\VistaSwitcher_1.1.5-setup.exe"
+		Invoke-WebRequest -UseBasicParsing "https://www.virustotal.com/static/bin/vtuploader2.2.exe" -OutFile "$ENV:TEMP\vtuploader2.2.exe"
+	
+	# No silent yet
+		Start-Process "$ENV:TEMP\Firefox Setup Stub 38.0.1.exe"
+	#	Start-Process "$ENV:TEMP\foobar2000_v1.3.8.exe"
+		Start-Process "$ENV:TEMP\GeForce_Experience_v2.4.3.31.exe"
+		Start-Process "$ENV:TEMP\GitHubSetup.exe"
+		Start-Process "$ENV:TEMP\honeyview.exe"
+		Start-Process "$ENV:TEMP\LGS_8.58.183_x64_Logitech.exe"
+		Start-Process "$ENV:TEMP\VistaSwitcher_1.1.5-setup.exe"
+		Start-Process "$ENV:TEMP\vtuploader2.2.exe"
 }
-
-Remove-Item $ENV:PUBLIC\silentinstall.ps1
