fb58aa0514ece9f67ffb0e09de41a905390df6fa
Link-Satonaka <Link-Satonaka@outlook.com>
2015-05-22 22:42:48 -0400

    updated, send to downloads folder

--- cygupdate.ps1
+++ cygupdate.ps1
@@ -1,6 +1,4 @@
-#todo- download to downloads folder?
-$webclient = New-Object System.Net.WebClient
 Write-Host "Downloading and installing cygwin..."
-$webclient.DownloadFile( "https://cygwin.com/setup-x86_64.exe", "$Env:TEMP\setup-x86_64.exe" )
+Invoke-WebRequest -UseBasicParsing -Uri "https://cygwin.com/setup-x86_64.exe" -OutFile "$Env:Downloads\setup-x86_64.exe"
 Start-Process "$Env:TEMP\setup-x86_64.exe" "--no-admin --quiet-mode -g -d -o -l c:\cygwin64\tmp -P irssi,curl,rsync,perl-Text-CharWidth,perl-Algorithm-Diff -s http://mirrors.kernel.org/sourceware/cygwin/"
 pause
