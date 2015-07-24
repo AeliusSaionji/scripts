9e62afb1f8ecf6a8d8c90152d478e0daa81da5c4
Link Satonaka <link.satonaka@outlook.com>
2015-07-24 06:00:38 -0400

    add ssh

--- cygupdate.ps1
+++ cygupdate.ps1
@@ -1,4 +1,4 @@
 Write-Host "Downloading and installing cygwin..."
 Invoke-WebRequest -UseBasicParsing -Uri "https://cygwin.com/setup-x86_64.exe" -OutFile "$Env:Downloads\setup-x86_64.exe"
-Start-Process "$Env:Downloads\setup-x86_64.exe" "--no-admin --quiet-mode -g -d -o -l c:\cygwin64\tmp -P irssi,curl,rsync,zsh,git,make,perl-Text-CharWidth,perl-Algorithm-Diff -s http://mirrors.kernel.org/sourceware/cygwin/"
+Start-Process "$Env:Downloads\setup-x86_64.exe" "--no-admin --quiet-mode -g -d -o -l c:\cygwin64\tmp -P irssi,openssh,curl,rsync,zsh,git,make,perl-Text-CharWidth,perl-Algorithm-Diff -s http://mirrors.kernel.org/sourceware/cygwin/"
 pause
