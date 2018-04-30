81bed9771db408b839d5ec3586cb63085f951886
Aelius <aeliussaionji@outlook.com>
2018-04-30 02:16:06 -0400

    improvements to tidystart

--- TidyStart.ps1
+++ TidyStart.ps1
@@ -1,32 +1,68 @@
-# Link Satonaka
+# Aelius Saionji
 # 1.0
 #	Initial release
 # 1.5
-# 	camelCase, variable $adminName & user printout
-# 	Revo Uninstaller exception
-# 	Steam folder exception
-# 	Backups now go to C:\tools\TidyStart, following the Chocolatey convention
+#	camelCase, variable $adminName & user printout
+#	Revo Uninstaller exception
+#	Steam folder exception
+#	Backups now go to C:\tools\TidyStart, following the Chocolatey convention
+# 1.8
+#	Increased readability + decreased line length
+#	Condense admin actions under one UAC prompt
+#	Don't make changes until end of script
+#	Make output text pretty
+#	
 # TODO
-# 	Choose from a list of users instead of having to type the username
-# 	Make text output pretty
-# 	Condense all actions under one UAC prompt
-# 	When Chocolatey updates Get-BinRoot, detect it via environment variable (keep OneGet support in mind)
+#	Numerical selection for root user
+#	If a folder only contains one shortcut: save shortcut, delete folder
+#	Use sane versions
+#	Reduce / eliminate new windows opened
+#	Reconsider use of heredocs- manually expanded script blocks are an option
 
-Write-Output "This script will move undesirable shortcuts from the start menu to $ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak with folder structure intact." 
-$fixRoot = Read-Host "First, copy all shortcuts from an admin's start menu to the global one? Yes to confirm"
+$startMenu = 'Microsoft\Windows\StartM~1'
+$roboArgs = '/S /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0'
+$backupDir = "$ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak"
+$cumulativeCmd = ""
+$cumulativeCmdAdmin = ""
+
+Write-Host -ForegroundColor Yellow "This script will move undesirable shortcuts from the start menu to $backupDir with folder structure intact."
+Write-Host -ForegroundColor Yellow "First, copy all shortcuts from an admin's start menu to the global one?"
+$fixRoot = Read-Host "Yes to confirm"
 if ($fixRoot -eq "yes") {
 	$userList = Get-ChildItem "$ENV:HOMEDRIVE\Users" -Exclude Public,Default*
-	Write-Output $userList.Name
-	$adminName = Read-Host "Please enter the username of the admin user exactly as it appears above"
-	Start-Process "robocopy" "$ENV:HOMEDRIVE\Users\$adminName\AppData\Roaming\Microsoft\Windows\StartM~1 $ENV:ProgramData\Microsoft\Windows\StartM~1 /S /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0" -Verb RunAs
+	Format-Table -Property Name -InputObject $userList
+	Write-Host -ForegroundColor Yellow "Please enter the username of the admin user exactly as it appears above."
+	$adminName = Read-Host "Name"
+	$cumulativeCmdAdmin += @"
+	Start-Process "robocopy" "$ENV:HOMEDRIVE\Users\$adminName\AppData\Roaming\$startMenu $ENV:ProgramData\$StartMenu $roboArgs";
+"@
 }
-$delUninst = Read-Host "Remove all shortcuts to uninstallers? Yes to confirm"
+
+Write-Host -ForegroundColor Yellow "Remove all shortcuts to uninstallers?"
+$delUninst = Read-Host "Yes to confirm"
 if ($delUninst -eq "yes") {
-	robocopy $ENV:APPDATA\Microsoft\Windows\StartM~1 $ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak\Local *Uninstall*.lnk /S /MOVE /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0 /XF "Revo Uninstaller Pro.lnk"
-	Start-Process "robocopy" "$ENV:ProgramData\Microsoft\Windows\StartM~1 $ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak\Global *Uninstall*.lnk /S /MOVE /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0 /XF ""Revo Uninstaller Pro.lnk""" -Verb RunAs
+	$cumulativeCmd += @"
+	Start-Process "robocopy" "$ENV:APPDATA\$startMenu     $backupDir\Local  *Uninstall*.lnk $roboArgs /MOVE /XF 'Revo Uninstaller Pro.lnk'";
+"@
+	$cumulativeCmdAdmin += @"
+	Start-Process "robocopy" "$ENV:ProgramData\$startMenu $backupDir\Global *Uninstall*.lnk $roboArgs /MOVE /XF 'Revo Uninstaller Pro.lnk'";
+"@
 }
-$delUrls = Read-Host "Remove all shortcuts to websites? Yes to confirm"
+
+Write-Host -ForegroundColor Yellow "Remove all shortcuts to websites?"
+$delUrls = Read-Host "Yes to confirm"
 if ($delUrls -eq "yes") {
-	robocopy $ENV:APPDATA\Microsoft\Windows\StartM~1 $ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak\Local *.url /S /MOVE /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0 /XD Steam
-	Start-Process "robocopy" "$ENV:ProgramData\Microsoft\Windows\StartM~1 $ENV:HOMEDRIVE\tools\TidyStart\StartMenuBak\Global *.url /S /MOVE /NP /NFL /NDL /NJH /NJS /NC /NS /XJ /R:0 /XD Steam" -Verb RunAs
+	$cumulativeCmd += @"
+	Start-Process "robocopy" "$ENV:APPDATA\$startMenu     $backupDir\Local  *.url $roboArgs /MOVE /XD Steam";
+"@
+	$cumulativeCmdAdmin += @"
+	Start-Process "robocopy" "$ENV:ProgramData\$startMenu $backupDir\Global *.url $roboArgs /MOVE /XD Steam";
+"@
 }
+
+$bytesAdmin = [System.Text.Encoding]::Unicode.GetBytes($cumulativeCmdAdmin)
+$bytes      = [System.Text.Encoding]::Unicode.GetBytes($cumulativeCmd)
+$encodedCommandAdmin = [Convert]::ToBase64String($bytesAdmin)
+$encodedCommand      = [Convert]::ToBase64String($bytes)
+Start-Process "powershell.exe" "-WindowStyle Hidden -EncodedCommand $encodedCommandAdmin" -Verb RunAs -Wait
+Start-Process "powershell.exe" "-WindowStyle Hidden -EncodedCommand $encodedCommand"
